0f15b4b48454da0ee261634de38bcf29eef17373
Nikita Malov <nikita.volam@gmail.com>
2023-03-08 22:29:44 +0200

    Add missing annotation to DepartmentCodeAlreadyExistsException

--- department-service/src/main/java/com/example/departmentservice/exception/DepartmentCodeAlreadyExistsException.java
+++ department-service/src/main/java/com/example/departmentservice/exception/DepartmentCodeAlreadyExistsException.java
@@ -1,5 +1,9 @@
 package com.example.departmentservice.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class DepartmentCodeAlreadyExistsException extends RuntimeException{
     public DepartmentCodeAlreadyExistsException(String message) {
         super(message);
